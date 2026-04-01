abf2d3d1bd4b38129b687133a9f2a0a7360e0623
aleksandr saliy <aleksandr.saliy@bwf.ru>
2026-04-02 00:46:18 +0300

    db

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,33 +1,26 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
-import { LoggerModule } from "./common/logger.module";
 import { PrismaModule } from "./common/prisma.module";
-import { DatabaseModule } from "./common/database.module";
 import { ProxyModule } from "./common/proxy.module";
 import { LegalEntitiesModule } from "./legal-entities/legal-entities.module";
 import { ChecksModule } from "./checks/checks.module";
 import { UsersModule } from "./users/users.module";
-import { ExportModule } from "./export/export.module";
 import { PersonsModule } from "./persons/persons.module";
 import { HealthController } from "./common/health.controller";
-import { RootController } from "./common/root.controller";
 
 @Module({
- imports: [
- ConfigModule.forRoot({
- isGlobal: true,
- envFilePath: ".env",
- }),
- LoggerModule,
- PrismaModule,
- DatabaseModule,
- ProxyModule,
- LegalEntitiesModule,
- ChecksModule,
- UsersModule,
- ExportModule,
- PersonsModule,
- ],
- controllers: [HealthController, RootController],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ".env",
+    }),
+    PrismaModule,
+    ProxyModule,
+    LegalEntitiesModule,
+    ChecksModule,
+    UsersModule,
+    PersonsModule,
+  ],
+  controllers: [HealthController],
 })
 export class AppModule {}
